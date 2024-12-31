92757268fe330fa59c906ae4bcc497372c3c52f6
Nitin Gangwani <97083939+Nitin-09@users.noreply.github.com>
2024-12-31 11:39:35 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -80,6 +80,11 @@ function Login(props) {
             <div className="mt-8">
                 <input className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full" type="submit" value="Login" />
             </div>
+        <div>
+                <span>Test User detail || </span>
+                <span>username : Test@gmail.com ||  </span>
+                <span>password : test@</span>
+            </div>
             {/* <div className="w-full flex items-center justify-between py-5">
                 <hr className="w-full bg-gray-400" />
                 <p className="text-base font-medium leading-4 px-2.5 text-gray-400">OR</p>
